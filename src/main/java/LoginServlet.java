dc94e9dc8c5494ae2a0d7071e4665da713322077
Songju Oh <songju.oh1@gmail.com>
2023-03-15 11:05:28 -0500

    worked on session and cookies exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,12 +3,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+
+        HttpSession session = request.getSession();
+
+        if(session.getAttribute("user")!=null){
+            response.sendRedirect("/profile");
+        }else{
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -17,6 +26,9 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+
+            HttpSession session = request.getSession();
+            session.setAttribute("user",username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/webapp/profile.jsp
+++ src/main/webapp/profile.jsp
@@ -1,4 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <jsp:include page="partials/head.jsp">
@@ -9,7 +11,15 @@
     <jsp:include page="partials/navbar.jsp" />
 
     <div class="container">
-        <h1>Viewing your profile.</h1>
+        <form action="/logout" method="POST">
+            <c:if test="${sessionScope.user != null}">
+                <h1 class="mb-5"> Hello, ${sessionScope.user}!</h1>
+                <h3>Viewing your profile...</h3>
+
+            </c:if>
+
+            <input type="submit" class="btn btn-primary btn-block mt-3" value="Log Out">
+        </form>
     </div>
 
 </body>
